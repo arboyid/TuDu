9e7459371cab3ff39baadee36f5223a9dcc7cc7a
Arboy <mardiansyahmsy@gmail.com>
2022-12-29 15:12:00 +0700

    Main Activity XML Done

---
+++ app/src/main/java/com/arboy/tudu/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.arboy.tudu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
