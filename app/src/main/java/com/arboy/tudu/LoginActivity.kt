39acde8f873c09f135d3dce20e8371631bc368dd
Arboy <mardiansyahmsy@gmail.com>
2022-12-29 18:47:40 +0700

    all off version xml has Done

--- app/src/main/java/com/arboy/tudu/LoginActivity.kt
+++ app/src/main/java/com/arboy/tudu/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.arboy.tudu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
+
+        btnBackLoginListener()
+    }
+    private fun btnBackLoginListener() {
+    L_back.setOnClickListener {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
     }
 }
--- app/src/main/java/com/arboy/tudu/MainActivity.kt
+++ app/src/main/java/com/arboy/tudu/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.arboy.tudu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat.startActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        btnLoginListener()
+        btnRegisterListener()
     }
+
+    private fun btnLoginListener() {
+        btn_1.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+    private fun btnRegisterListener() {
+        btn_2.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
 }
